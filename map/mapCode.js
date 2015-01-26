c007fc2e6370450f111f3c4e511378432f1824c1
Brandon Kelley <bmk4993@gmail.com>
2015-01-25 22:52:21 -0500

    Finished implementation for makeMap (Close #15)

--- map/mapCode.js
+++ map/mapCode.js
@@ -6,10 +6,46 @@
 // Do not change the name of this function
 var makeMap = function() {
 // All your code will go inside this function
-   // This object should contain the methods you want to expose:
-   var o = {};
    // Use this object to store the key-value pairs:
    var storedPairs = {};
+
+   // This object should contain the methods you want to expose:
+   var o = {
+      has: function(key) {
+         return storedPairs.hasOwnProperty(key);
+      },
+      lookup: function(key) {
+         var temp = o.has(key);
+         if(temp === true) {
+            return storedPairs[key];
+         } else {
+            throw new Error("Error: Method: lookup(); no key-value pair with key: " + key);
+         }
+      },
+      add: function(key, value) {
+         if(o.has(key)){
+            throw new Error("Error: Method: add(); map already has key: " + key);
+         } else {
+            storedPairs[key] = value;
+            return storedPairs;
+         }
+      },
+      update: function(key, value) {
+         if(o.has(key) === false){
+            throw new Error("Error: Method: update(); map doesn't have key: " + key);
+         } else {
+            storedPairs[key] = value;
+            return storedPairs;
+         }
+      },
+      remove: function(key) {
+         if(o.has(key) === false){
+            throw new Error("Error: Method: remove(); map doesn't have key: " + key);
+         } else {
+            delete storedPairs[key];
+         }
+      }
+   };
 
    // Add initialization code here
 
